5fe7f96eb480de3b6a7f378fb28a1c5ca957323e
Carolina Noriega Rincon <112576449+cnoriegar26@users.noreply.github.com>
2023-02-13 09:50:04 -0500

    Delete 01-Introduccion-JavaScript directory

--- 01-Introduccion-JavaScript/mi-primer-proyecto-de-node/index.js
+++
@@ -1,8 +0,0 @@
-//Este es el mensaje de bienvenida
-console.log("Esta es la puerta de entrada al proyecto");
-
-//Esta es la parte intermedia de nuestro proyecto
-//En esta parte realizaremos los chequeos necesarios
-//Por ultimo vamos a la ultima linea de saludo
-//Este va ser el mensaje que se envia para despedir 
-console.log("Adios");
