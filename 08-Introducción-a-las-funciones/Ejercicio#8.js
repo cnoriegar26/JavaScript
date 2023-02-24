8a1c8b6548bd2de691f2d6153bc0ebc4fee2d1f1
Carolina Noriega <112576449+cnoriegar26@users.noreply.github.com>
2023-02-24 15:29:24 -0500

    Add files via upload

---
+++ 08-Introducción-a-las-funciones/Ejercicio#8.js
@@ -0,0 +1,14 @@
+function devuelvaTrue() {
+    return true
+}
+
+async function Promesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
